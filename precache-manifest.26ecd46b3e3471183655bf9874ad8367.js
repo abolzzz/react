cccdf7e3bff1ac96cdc19982df00390b5cd2b070
abolzzz <kkalnins8@gmail.com>
2022-05-18 23:31:56 +0300

    Updates

---
+++ precache-manifest.26ecd46b3e3471183655bf9874ad8367.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "13367d9558b3553c4677",
+    "url": "/react/static/css/main.6c557ce8.chunk.css"
+  },
+  {
+    "revision": "13367d9558b3553c4677",
+    "url": "/react/static/js/main.13367d95.chunk.js"
+  },
+  {
+    "revision": "ac2e7b5deb3dac76bf0e",
+    "url": "/react/static/css/1.371b8a45.chunk.css"
+  },
+  {
+    "revision": "ac2e7b5deb3dac76bf0e",
+    "url": "/react/static/js/1.ac2e7b5d.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "a0d774d2b07e683e7926899afb3ebb66",
+    "url": "/react/static/media/me6.a0d774d2.png"
+  },
+  {
+    "revision": "24b802b0d3310567dcf587228c10fb9a",
+    "url": "/react/static/media/pepega.24b802b0.jpg"
+  },
+  {
+    "revision": "a04855aa0e38a6e8c4b175c2318b6fad",
+    "url": "/react/static/media/profile.a04855aa.jpg"
+  },
+  {
+    "revision": "bfbbb8a4cae2784a9622961145f11c15",
+    "url": "/react/static/media/profile5.bfbbb8a4.jpg"
+  },
+  {
+    "revision": "ddf249605ad1c1c58cd74d07ae52bd57",
+    "url": "/react/static/media/profile3.ddf24960.jpg"
+  },
+  {
+    "revision": "c685367f1406e6abdd37a121d7c2f4c2",
+    "url": "/react/static/media/profile4.c685367f.jpg"
+  },
+  {
+    "revision": "698ddb5254a8bfc90b13cf0e1064179a",
+    "url": "/react/index.html"
+  }
+];
